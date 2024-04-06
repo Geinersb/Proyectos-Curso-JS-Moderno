07b5daf2556d2bc554db4290435df2c7e98fbd56
Geinersb <geinersb20@gmail.com>
2024-04-06 13:25:36 -0600

    se configura insertar datos en db en proyecto crmindexdb

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -43,6 +43,45 @@
 
             return;
         }
+
+        //CREAR UN OBJETO CON LA INFORMACION DEL FORMULARIO 
+            const cliente = {
+                nombre: nombre,
+                email: email,
+                telefono: telefono,
+                empresa: empresa,
+                //si no tenemos una bd podemos generar el id con esto 
+                id: Date.now()
+            }
+            
+            crearNuevoCliente(cliente);
+    }
+
+
+    function crearNuevoCliente(cliente){
+        //con esto llamamos la instancia de la base de datos para utilizar las transacciones 
+        const transaction = DB.transaction(['crm'],'readwrite');
+
+        //definimos el object store y le decimos que base a utilizar
+        const objectStore = transaction.objectStore('crm');
+
+        //aqui agregamos el registro 
+        objectStore.add(cliente);
+
+        //si hay un error
+        transaction.onerror = function(){
+            imprimirAlerta('Hubo un error a la hora de ingresar registro','error');         
+        };
+        //si fue correcto 
+        transaction.oncomplete = function(){
+            imprimirAlerta('Registro ingresado correctamente','sucess');
+
+            //esto me desvia a la pagina de clientes index
+            setTimeout(() => {
+                    window.location.href = 'index.html';
+            }, 3000);
+        }
+
     }
 
     function imprimirAlerta(mensaje, tipo) {
