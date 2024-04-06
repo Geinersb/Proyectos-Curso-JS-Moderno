83a6d4c2a93c4f5e7a33f6ebafec42e95166428c
Geinersb <geinersb20@gmail.com>
2024-04-06 17:24:34 -0600

    se finaliza eliminacion de datos y proyecto crmIndexdb

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -2,6 +2,8 @@
 
     //variable global 
     let DB;
+     //llamamos el id de la tabla del listado del html 
+     const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
 
@@ -11,7 +13,36 @@
         if (window.indexedDB.open('crm', 1)) {
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click',eliminarRegistro);
     });
+
+    function eliminarRegistro (e){
+        //con esto busca si le estamos dando cliente a un elemento del listadoclientes con la clase eliminar
+        if(e.target.classList.contains('eliminar')){
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm('Deseas eliminar este cliente?');
+
+            if(confirmar){
+                const transaction = DB.transaction(['crm'],'readwrite');
+                const objectStore =transaction.objectStore('crm');
+
+                //con esto eliminamos el registro del ID 
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function(){
+                    console.log('Eliminado..');
+                    e.target.parentElement.parentElement.remove();
+                }
+                
+                transaction.onerror = function(){
+                    console.log('Hubo un error');
+                }
+
+            }
+        }
+    }
 
     //CREA LA BASE DE DATOS DE INDEXDB
     function crearDB() {
@@ -69,8 +100,7 @@
                 if (cursor) {
                     const { nombre, empresa, email, telefono, id } = cursor.value;
 
-                    //llamamos el id de la tabla del listado del html 
-                    const listadoClientes = document.querySelector('#listado-clientes');
+                   
 
                     //insertamos la tabla en el HTML 
                     listadoClientes.innerHTML += ` <tr>
@@ -86,7 +116,7 @@
                     </td>
                     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                         <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                        <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                        <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                     </td>
                 </tr>
             `;
